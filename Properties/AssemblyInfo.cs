37e71a2c6288c6d6a6cdbb9f0325d154bf686226
HP <HP@DESKTOP-EQ9MNTC>
2022-09-29 21:15:28 +0400

    Добавьте файлы проекта.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("childrenn")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("childrenn")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8b10696b-f6e1-47c0-8d5d-4e5840546440")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+Children FirstChild = new Children();
+Children SecondChild = new Children();
+
+FirstChild.Print();         
+SecondChild.Print();         
+
+class Children
+{
+    public string name;
+    public string surname;
+    public int age;
+    
+    private void Enter()
+    {
+        Console.Write("Enter name: ");
+        name = Console.ReadLine();
+        Console.Write("Enter surname: ");
+        surname = Console.ReadLine();
+        Console.Write("Enter age: ");
+        age = Console.ReadLine();
+    }
+    
+    public void Print()
+    {
+        Console.WriteLine($"Name: {name} Surname: {surname} Age: {age}");
+    }
+}
